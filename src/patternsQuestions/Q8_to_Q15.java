c97b8d2ea5cfd03f14e3fa8dbfb0b94aa4089923
Khalid <klood_12@hotmail.com>
2023-09-01 14:48:00 +0400

    I just add comment which is the pattern above the pattern10 method

--- src/patternsQuestions/Q8_to_Q15.java
+++ src/patternsQuestions/Q8_to_Q15.java
@@ -53,6 +53,15 @@ public class Q8_to_Q15 {
 		}
 	}
 	
+	// Pattern 10
+	
+		/*
+				    *
+			       * *
+			      * * *
+			     * * * *
+			    * * * * *
+	    */
 	static void pattern10(int n) {
 		for (int row = 1; row <= n; row++) {
 			for (int space = 1; space <= n - row; space++) {
