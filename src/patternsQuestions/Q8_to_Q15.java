29b8235d854b2dd6a7c9f45871c4bd8d14b0243e
Khalid <klood_12@hotmail.com>
2023-09-01 14:34:45 +0400

    I solved question 9 of the patterns questions

--- src/patternsQuestions/Q8_to_Q15.java
+++ src/patternsQuestions/Q8_to_Q15.java
@@ -3,7 +3,10 @@ package patternsQuestions;
 public class Q8_to_Q15 {
 
 	public static void main(String[] args) {
+		/*
 		pattern8(5);
+		pattern9(5);
+		*/
 	}
 	// Pattern 8
 	
@@ -24,6 +27,27 @@ public class Q8_to_Q15 {
 				System.out.print("*");
 			}
 			// When row is printed, we need a new line
+			System.out.println();
+		}
+	}
+	// Pattern 9
+	
+	/*
+			   *********
+			    *******
+			     *****
+			      ***
+			       *
+    */
+	
+	static void pattern9(int n) {
+		for (int row = 1; row <= n; row++) {
+			for (int space = 1; space <= row - 1; space++) {
+				System.out.print(" ");
+			}
+				for (int col = row; col <= (2 * n) - row; col++) {
+					System.out.print("*");
+				}
 			System.out.println();
 		}
 	}
